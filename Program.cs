c537517b7dfc976a5b81e0abdac4caf5deb161a5
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-29 15:46:04 -0300

    :sparkles: feat: cria os cálculos para o programa entender quando usuário deseja contar segundos ou minutos

--- Program.cs
+++ Program.cs
@@ -21,10 +21,15 @@ namespace StopWatch
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
             int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
 
-            Console.WriteLine(type);
-            Console.WriteLine(time);
+            if(type == 'm')
+                multiplier = 60;
 
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
         }
 
         static void Start(int time)
@@ -42,6 +47,7 @@ namespace StopWatch
             Console.Clear();
             Console.WriteLine("StopWatch finalizado");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
